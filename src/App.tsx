a964cda9d3414b73f5b56d0719c29e7016c4363c
Harry Banda <harry.banda@mudaall.com>
2022-03-02 12:06:41 +0200

    Added unsplashed API and env vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,23 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+const { REACT_APP_ACCESS_KEY } = process.env;
+const UNSPLASH_API = "https://api.unsplash.com/photos/?client_id=";
+
 function App() {
-  return <div></div>;
+  const [photos, setPhotos] = useState<any[]>([]);
+
+  useEffect(() => {
+    axios
+      .get(UNSPLASH_API + REACT_APP_ACCESS_KEY)
+      .then(function (response) {
+        setPhotos(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {});
+  }, []);
+
+  return <div>{JSON.stringify(photos)}</div>;
 }
 export default App;
